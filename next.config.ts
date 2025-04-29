5182bd79830896a4f5c6960b23c356d28026203a
gyulrangdev <gyulrangdev@gmail.com>
2025-04-29 17:09:21 +0900

    Chore: svgr 웹팩 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    // SVG 파일을 React 컴포넌트로 사용하기 위한 설정
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   turbopack: {
     rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
       },
     },
   },
