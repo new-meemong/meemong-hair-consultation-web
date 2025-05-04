fdde302c156e0eadc0c4534de08fcc9f12f29720
gyulrangdev <gyulrangdev@gmail.com>
2025-05-04 17:15:52 +0900

    Chore: 예시 이미지를 위한 domain 허용

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['picsum.photos'],
+  },
   webpack(config) {
     // SVG 파일을 React 컴포넌트로 사용하기 위한 설정
     config.module.rules.push({
