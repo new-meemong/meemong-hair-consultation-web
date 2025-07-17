e22b4d107cb1e090d4b76559091e93efd136bb8d
gaeun <knospe1@naver.com>
2025-07-17 15:46:28 +0900

    refactor: 더 이상 사용하지 않는 파일 삭제

--- src/features/posts/hooks/use-delete-post.tsx
+++
@@ -1,9 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-
-export default function useDeletePost() {
-  const { mutate: deletePost, isPending } = useMutation({
-    mutationFn: (postId: number) => deletePost(postId),
-  });
-
-  return { deletePost, isPending };
-}
