24a6251573281a00075330834e33ebaebb8d2665
gaeun <knospe1@naver.com>
2025-07-17 17:39:41 +0900

    fix: checkbox ui 수정

--- src/shared/ui/checkbox.tsx
+++ src/shared/ui/checkbox.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import * as React from 'react';
 
+import CheckboxIcon from '@/assets/icons/checkbox.svg';
 import { cn } from '@/shared/lib/utils';
-import { Check } from 'lucide-react';
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
@@ -11,14 +11,14 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      'peer h-4 w-4 shrink-0 rounded-2 border border-border-default focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-label-default data-[state=checked]:border-0',
+      'peer size-5 rounded-2 border border-border-default focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-0',
       className,
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator className={cn('items-center justify-center text-current')}>
+    <CheckboxPrimitive.Indicator>
       {/* 체크박스 아이콘 또는 내용 */}
-      <Check className="h-4 w-4 text-white" />
+      <CheckboxIcon />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
