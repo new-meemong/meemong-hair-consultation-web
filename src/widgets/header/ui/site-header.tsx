225e9e141fb135e0c2dd7be4523a0ae127265109
gaeun <knospe1@naver.com>
2025-07-27 22:26:30 +0900

    fix: site-header ui 수정

--- src/widgets/header/ui/site-header.tsx
+++ src/widgets/header/ui/site-header.tsx
@@ -33,10 +33,10 @@ export const SiteHeader = ({
         /* 백버튼이 있는 헤더 디자인 (가운데 정렬 제목) */
         <div className="flex items-center justify-between w-full">
           <button onClick={handleBackClick} aria-label="뒤로 가기">
-            <ChevronLeftIcon className="h-6 w-6 fill-rank-second" />
+            <ChevronLeftIcon className="h-7 w-7 fill-rank-second" />
           </button>
           <h1 className="typo-title-3-semibold flex-1 text-center">{title}</h1>
-          {rightComponent}
+          {rightComponent ? rightComponent : <div className="w-7" />}
         </div>
       ) : (
         /* 메인 페이지 헤더 디자인 (좌측 정렬 제목) */
