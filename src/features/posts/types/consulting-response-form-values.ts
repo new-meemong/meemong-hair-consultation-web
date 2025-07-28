90becb313430053c12791ca1485418dea28ad44a
gaeun <knospe1@naver.com>
2025-07-28 16:25:20 +0900

    fix: 컨설팅 답글달기 step3 매장상담필요 옵션 추가

--- src/features/posts/types/consulting-response-form-values.ts
+++ src/features/posts/types/consulting-response-form-values.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import { CONSULTING_RESPONSE_FORM_FIELD_NAME } from '../constants/consulting-response-form-field-name';
 import { FACE_TYPE } from '../ui/consulting-response-form/consulting-response-form-step-2';
+import { HAIR_TYPE } from '../ui/consulting-response-form/consulting-response-form-step-3';
 
 const FACE_TYPE_VALUES = [
   FACE_TYPE.OVAL,
@@ -11,10 +12,17 @@ const FACE_TYPE_VALUES = [
   FACE_TYPE.ROUND,
 ] as const;
 
+const HAIR_TYPE_VALUES = [
+  HAIR_TYPE.MALIGNANT_CURLY,
+  HAIR_TYPE.CURLY,
+  HAIR_TYPE.SEMI_CURLY,
+  HAIR_TYPE.STRAIGHT,
+] as const;
+
 export const consultingResponseFormSchema = z.object({
   [CONSULTING_RESPONSE_FORM_FIELD_NAME.option1]: z.string().optional(),
   [CONSULTING_RESPONSE_FORM_FIELD_NAME.option2]: z.enum(FACE_TYPE_VALUES),
-  [CONSULTING_RESPONSE_FORM_FIELD_NAME.option3]: z.string().optional(),
+  [CONSULTING_RESPONSE_FORM_FIELD_NAME.option3]: z.enum(HAIR_TYPE_VALUES).nullable().optional(),
   [CONSULTING_RESPONSE_FORM_FIELD_NAME.option4]: z.string().optional(),
   [CONSULTING_RESPONSE_FORM_FIELD_NAME.option5]: z.string().optional(),
   [CONSULTING_RESPONSE_FORM_FIELD_NAME.option6]: z.string().optional(),
