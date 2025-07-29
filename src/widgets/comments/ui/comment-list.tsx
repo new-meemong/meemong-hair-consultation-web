db36f765ec92c3144ad1851ec105f591ef59232b
gaeun <knospe1@naver.com>
2025-07-29 14:48:27 +0900

    feat: 댓글조회 무한스크롤 기능 추가

--- src/widgets/comments/ui/comment-list.tsx
+++ src/widgets/comments/ui/comment-list.tsx
@@ -2,21 +2,28 @@
 
 import type { CommentWithReplyStatus } from '@/entities/comment/model/comment';
 import CommentListItem from './comment-list-item';
+import { useIntersectionObserver } from '@/shared/hooks/use-intersection-observer';
 
 interface CommentListProps {
   comments: CommentWithReplyStatus[];
+  fetchNextPage: () => void;
   onAddComment: (content: string, isPrivate: boolean, parentId?: string) => void;
   onEditComment?: (commentId: string, content: string) => void;
   onDeleteComment?: (commentId: string) => void;
   onReportComment?: (commentId: string) => void;
 }
 
-export function CommentList({ comments }: CommentListProps) {
+export function CommentList({ comments, fetchNextPage }: CommentListProps) {
+  const observerRef = useIntersectionObserver({
+    onIntersect: fetchNextPage,
+  });
+
   return (
     <>
       {comments.map((comment) => (
         <CommentListItem key={comment.id} comment={comment} />
       ))}
+      <div className="h-1" ref={observerRef} />
     </>
   );
 }
