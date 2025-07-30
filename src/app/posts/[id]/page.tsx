bcd5fb6b28b64b2956da851f5a7160b299f03d23
gaeun <knospe1@naver.com>
2025-07-30 18:25:07 +0900

    fix: comment container 구조 변경

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useCallback } from 'react';
 import { useAuthContext } from '@/features/auth/context/auth-context';
 import { CommentContainer } from '@/widgets/comments/ui/comment-container';
 import useGetPostDetail from '@/features/posts/api/use-get-post-detail';
@@ -9,6 +10,8 @@ import { USER_GUIDE_KEYS } from '@/shared/constants/local-storage';
 import useGuidePopup from '@/shared/hooks/use-guide-popup';
 import { SiteHeader } from '@/widgets/header';
 import { useParams } from 'next/navigation';
+import { useCommentFormState } from '@/features/comments/hooks/use-comment-form-state';
+import { CommentForm, type CommentFormValues } from '@/features/comments/ui/comment-form';
 
 export default function PostDetailPage() {
   const { isUserDesigner, user } = useAuthContext();
@@ -21,6 +24,20 @@ export default function PostDetailPage() {
 
   const isWriter = postDetail?.hairConsultPostingCreateUserId === user.id;
 
+  const { commentFormState, textareaRef, isCommentCreating, isCommentUpdating, handlers } =
+    useCommentFormState(postId?.toString() ?? '');
+
+  const handleContainerClick = useCallback(() => {
+    handlers.resetCommentState();
+  }, [handlers]);
+
+  const handleCommentFormSubmit = useCallback(
+    (data: CommentFormValues, options: { onSuccess: () => void }) => {
+      handlers.handleCommentFormSubmit(data, options);
+    },
+    [handlers],
+  );
+
   if (!postId) return null;
 
   return (
@@ -30,12 +47,31 @@ export default function PostDetailPage() {
         showBackButton
         rightComponent={isWriter && <PostDetailMoreButton postId={postId.toString()} />}
       />
-      <div className="flex-1 overflow-y-auto">
+      <div className="flex-1 overflow-y-auto" onClick={handleContainerClick}>
         {postDetail && (
           <PostDetailContainer postDetail={postDetail}>
-            <CommentContainer postId={postId.toString()} />
+            <CommentContainer
+              postId={postId.toString()}
+              commentFormState={commentFormState}
+              handlers={{
+                ...handlers,
+                handleCommentFormSubmit,
+              }}
+            />
           </PostDetailContainer>
         )}
+      </div>
+      <div className="f bg-white shadow-strong">
+        <div className="max-w-[600px] mx-auto">
+          <CommentForm
+            onSubmit={handlers.handleCommentFormSubmit}
+            isReply={commentFormState.state === 'reply'}
+            commentId={commentFormState.commentId}
+            content={commentFormState.content}
+            isPending={isCommentCreating || isCommentUpdating}
+            textareaRef={textareaRef}
+          />
+        </div>
       </div>
     </div>
   );
--- src/widgets/comments/ui/comment-container.tsx
+++ src/widgets/comments/ui/comment-container.tsx
@@ -1,16 +1,33 @@
-import { useCallback } from 'react';
-import { CommentList } from '../../../features/comments/ui/comment-list';
-import { CommentForm } from '../../../features/comments/ui/comment-form';
-import useGetPostComments from '../../../features/comments/api/use-get-post-comments';
-import { useCommentFormState } from '../../../features/comments/hooks/use-comment-form-state';
 import type { CommentWithReplyStatus } from '@/entities/comment/model/comment';
-import { useAuthContext } from '@/features/auth/context/auth-context';
+import type { CommentFormState } from '@/features/comments/types/comment-form-state';
+import { useCallback } from 'react';
+import useGetPostComments from '../../../features/comments/api/use-get-post-comments';
+import { CommentList } from '../../../features/comments/ui/comment-list';
 
-export const CommentContainer = ({ postId }: { postId: string }) => {
-  const { isUserDesigner } = useAuthContext();
-  const { commentFormState, textareaRef, isCommentCreating, isCommentUpdating, handlers } =
-    useCommentFormState(postId);
+type CommentActionHandlers = {
+  handleReplyClick: (commentId: number) => void;
+  handleDeleteComment: (commentId: number) => void;
+  handleEditComment: (commentId: number, comments: CommentWithReplyStatus[]) => void;
+  handleCommentFormSubmit: (
+    data: {
+      content: string;
+      isVisibleToModel: boolean;
+      parentCommentId: string | null;
+    },
+    options: {
+      onSuccess: () => void;
+    },
+  ) => void;
+  resetCommentState: () => void;
+};
 
+type CommentContainerProps = {
+  postId: string;
+  commentFormState: CommentFormState;
+  handlers: CommentActionHandlers;
+};
+
+export const CommentContainer = ({ postId, commentFormState, handlers }: CommentContainerProps) => {
   const { data, hasNextPage, isFetchingNextPage, fetchNextPage } = useGetPostComments(postId);
 
   const comments: CommentWithReplyStatus[] =
@@ -27,35 +44,16 @@ export const CommentContainer = ({ postId }: { postId: string }) => {
     }
   }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
-  const formHeight = isUserDesigner ? 64 + 48 : 64;
-  const bottomPadding = `pb-[${formHeight}px]`;
-
   return (
-    <>
-      <div className={bottomPadding}>
-        <CommentList
-          comments={comments}
-          fetchNextPage={handleFetchNextPage}
-          onReplyClick={handlers.handleReplyClick}
-          focusedCommentId={commentFormState.commentId}
-          onDelete={handlers.handleDeleteComment}
-          onEdit={(commentId) => handlers.handleEditComment(commentId, comments)}
-          onReport={() => {}}
-          onTriggerClick={handlers.resetCommentState}
-        />
-      </div>
-      <div className="fixed bottom-0 left-0 right-0 bg-white shadow-strong">
-        <div className="max-w-[600px] mx-auto">
-          <CommentForm
-            onSubmit={handlers.handleCommentFormSubmit}
-            isReply={commentFormState.state === 'reply'}
-            commentId={commentFormState.commentId}
-            content={commentFormState.content}
-            isPending={isCommentCreating || isCommentUpdating}
-            textareaRef={textareaRef}
-          />
-        </div>
-      </div>
-    </>
+    <CommentList
+      comments={comments}
+      fetchNextPage={handleFetchNextPage}
+      onReplyClick={handlers.handleReplyClick}
+      focusedCommentId={commentFormState.commentId}
+      onDelete={handlers.handleDeleteComment}
+      onEdit={(commentId) => handlers.handleEditComment(commentId, comments)}
+      onReport={() => {}}
+      onTriggerClick={handlers.resetCommentState}
+    />
   );
 };
