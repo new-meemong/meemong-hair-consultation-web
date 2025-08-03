f2c1ffa2a0946d4d2d9bd5a939319c25cb016043
gaeun <knospe1@naver.com>
2025-08-03 13:10:10 +0900

    refactor: skin-color-label 컴포넌트 분리

--- src/features/posts/ui/consulting-form/consulting-post-form/consulting-post-form-step-5.tsx
+++ src/features/posts/ui/consulting-form/consulting-post-form/consulting-post-form-step-5.tsx
@@ -1,49 +1,14 @@
 import { CONSULTING_POST_FORM_FIELD_NAME } from '../../../constants/consulting-post-form-field-name';
 import type { ConsultingFormOption } from '../../../types/consulting-form-option';
+import SkinColorLabel, { SKIN_COLOR_OPTION } from '../../skin-color-label';
 import ConsultingFormOptionList from '../consulting-form-option-list';
 
-const SKIN_COLOR = {
-  VERY_BRIGHT: 'veryBright',
-  BRIGHT: 'bright',
-  NORMAL: 'normal',
-  DARK: 'dark',
-  VERY_DARK: 'veryDark',
-};
-
-function Label({ label, color }: { label: string; color: string }) {
-  return (
-    <div className="flex gap-3 items-center">
-      <div
-        className="w-6 h-6 border-1 border-border-strong rounded-2"
-        style={{ backgroundColor: color }}
-      />
-      <p>{label}</p>
-    </div>
-  );
-}
-
-const SKIN_COLOR_OPTIONS: ConsultingFormOption[] = [
-  {
-    label: <Label label="매우 밝음" color="#FFFCFB" />,
-    value: SKIN_COLOR.VERY_BRIGHT,
-  },
-  {
-    label: <Label label="밝음" color="#FFF6F1" />,
-    value: SKIN_COLOR.BRIGHT,
-  },
-  {
-    label: <Label label="보통" color="#FDF0E9" />,
-    value: SKIN_COLOR.NORMAL,
-  },
-  {
-    label: <Label label="어두움" color="#EFDDD4" />,
-    value: SKIN_COLOR.DARK,
-  },
-  {
-    label: <Label label="매우 어두움" color="#EAD3C8" />,
-    value: SKIN_COLOR.VERY_DARK,
-  },
-];
+const SKIN_COLOR_OPTIONS: ConsultingFormOption[] = Object.values(SKIN_COLOR_OPTION).map(
+  (option) => ({
+    label: <SkinColorLabel type={option.value} />,
+    value: option.value,
+  }),
+);
 
 export default function ConsultingPostFormStep5() {
   return (
