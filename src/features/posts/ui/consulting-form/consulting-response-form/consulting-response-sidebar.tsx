34c865e88cb286702874cf92e2ccd80eb40c4a9b
gaeun <knospe1@naver.com>
2025-08-04 23:29:18 +0900

    feat: 컨설팅 답변 사이드바 탭 메뉴 추가

--- src/features/posts/ui/consulting-form/consulting-response-form/consulting-response-sidebar.tsx
+++ src/features/posts/ui/consulting-form/consulting-response-form/consulting-response-sidebar.tsx
@@ -1,6 +1,38 @@
 import { MULTI_STEP_FORM_PORTAL_ID } from '@/shared/ui/multi-step-form';
 import { createPortal } from 'react-dom';
 import CloseIcon from '@/assets/icons/close.svg';
+import GalleryIcon from '@/assets/icons/gallery.svg';
+import StarIcon from '@/assets/icons/star.svg';
+import ArticleIcon from '@/assets/icons/article.svg';
+import { ToggleChip, ToggleChipGroup } from '@/shared';
+import { useState } from 'react';
+import type { ValueOf } from '@/shared/type/types';
+
+const CONSULTING_RESPONSE_SIDEBAR_TAB_VALUE = {
+  CURRENT_STATE: 'currentState',
+  DESIRED_STYLE: 'desiredStyle',
+  ADDITIONAL_INFO: 'additionalInfo',
+} as const;
+
+const CONSULTING_RESPONSE_SIDEBAR_TAB = {
+  [CONSULTING_RESPONSE_SIDEBAR_TAB_VALUE.CURRENT_STATE]: {
+    label: '현재 상태',
+    value: CONSULTING_RESPONSE_SIDEBAR_TAB_VALUE.CURRENT_STATE,
+    icon: <GalleryIcon className="size-4 fill-label-info group-data-[state=on]:fill-white" />,
+  },
+  [CONSULTING_RESPONSE_SIDEBAR_TAB_VALUE.DESIRED_STYLE]: {
+    label: '추구미',
+    value: CONSULTING_RESPONSE_SIDEBAR_TAB_VALUE.DESIRED_STYLE,
+    icon: <StarIcon className="size-4 fill-label-info group-data-[state=on]:fill-white" />,
+  },
+  [CONSULTING_RESPONSE_SIDEBAR_TAB_VALUE.ADDITIONAL_INFO]: {
+    label: '추가 정보',
+    value: CONSULTING_RESPONSE_SIDEBAR_TAB_VALUE.ADDITIONAL_INFO,
+    icon: <ArticleIcon className="size-4 fill-label-info group-data-[state=on]:fill-white" />,
+  },
+} as const;
+
+const CONSULTING_RESPONSE_SIDEBAR_TABS = Object.values(CONSULTING_RESPONSE_SIDEBAR_TAB);
 
 type ConsultingResponseSidebarProps = {
   isOpen: boolean;
@@ -11,6 +43,12 @@ export default function ConsultingResponseSidebar({
   isOpen,
   onClose,
 }: ConsultingResponseSidebarProps) {
+  const [activeTab, setActiveTab] = useState(CONSULTING_RESPONSE_SIDEBAR_TABS[0].value);
+
+  const handleTabChange = (tab: ValueOf<typeof CONSULTING_RESPONSE_SIDEBAR_TAB_VALUE>) => {
+    setActiveTab(tab);
+  };
+
   return createPortal(
     <div className={`absolute inset-0 z-50 ${!isOpen ? 'pointer-events-none' : ''}`}>
       <div
@@ -25,10 +63,23 @@ export default function ConsultingResponseSidebar({
           ${isOpen ? 'translate-x-0' : 'translate-x-full'}
         `}
       >
-        <button type="button" className="fixed top-5 right-5" onClick={onClose}>
-          <CloseIcon className="size-7" />
-        </button>
-        {/* 사이드바 내용 */}
+        <div className="flex flex-col gap-3 py-5">
+          <button type="button" className="self-end pr-5" onClick={onClose}>
+            <CloseIcon />
+          </button>
+          <ToggleChipGroup className="flex overflow-x-auto scrollbar-hide px-5">
+            {CONSULTING_RESPONSE_SIDEBAR_TABS.map(({ value, icon, label }) => (
+              <ToggleChip
+                key={value}
+                icon={icon}
+                pressed={activeTab === value}
+                onPressedChange={() => handleTabChange(value)}
+              >
+                {label}
+              </ToggleChip>
+            ))}
+          </ToggleChipGroup>
+        </div>
       </div>
     </div>,
     document.getElementById(MULTI_STEP_FORM_PORTAL_ID) || document.body,
