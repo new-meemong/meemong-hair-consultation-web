b3cf173018e196ed234386de4fc1201c96b1d0af
gaeun <knospe1@naver.com>
2025-08-04 15:31:52 +0900

    refactor: 얼굴 타입, 헤어 타입 상수 분리

--- src/features/posts/ui/consulting-form/consulting-response-form/consulting-response-form-step-2.tsx
+++ src/features/posts/ui/consulting-form/consulting-response-form/consulting-response-form-step-2.tsx
@@ -1,35 +1,9 @@
+import { HAIR_TYPE_OPTIONS } from '@/features/posts/constants/hair-type';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { CONSULTING_RESPONSE_FORM_FIELD_NAME } from '../../../constants/consulting-response-form-field-name';
-import type { ConsultingFormOption } from '../../../types/consulting-form-option';
 import type { ConsultingResponseFormValues } from '../../../types/consulting-response-form-values';
 import ConsultingFormOptionList from '../consulting-form-option-list';
 import ConsultingResponseFormOptionNeedConsultation from './consulting-response-form-option-need-consultation';
-
-export const HAIR_TYPE = {
-  MALIGNANT_CURLY: 'malignantCurly',
-  CURLY: 'curly',
-  SEMI_CURLY: 'semiCurly',
-  STRAIGHT: 'straight',
-} as const;
-
-const HAIR_TYPE_OPTIONS: ConsultingFormOption[] = [
-  {
-    label: '악성곱슬',
-    value: HAIR_TYPE.MALIGNANT_CURLY,
-  },
-  {
-    label: '곱슬',
-    value: HAIR_TYPE.CURLY,
-  },
-  {
-    label: '반곱슬',
-    value: HAIR_TYPE.SEMI_CURLY,
-  },
-  {
-    label: '직모',
-    value: HAIR_TYPE.STRAIGHT,
-  },
-];
 
 export default function ConsultingResponseFormStep2() {
   const { control, setValue } = useFormContext<ConsultingResponseFormValues>();
