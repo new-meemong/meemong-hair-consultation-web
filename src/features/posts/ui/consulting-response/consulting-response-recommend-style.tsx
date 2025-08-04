b00fdd4cd625f3d77432b97592205b98399989a9
gaeun <knospe1@naver.com>
2025-08-04 16:12:56 +0900

    feat: 컨설팅 답변 스타일 추천 ui 추가

--- src/features/posts/ui/consulting-response/consulting-response-recommend-style.tsx
+++ src/features/posts/ui/consulting-response/consulting-response-recommend-style.tsx
@@ -1,15 +1,28 @@
 import { Separator } from '@/shared';
-import ConsultingResponseBangStyle from './consulting-response-bang-style';
 import { BANG_STYLE } from '../../constants/bang-style';
+import ConsultingResponseBangStyle from './consulting-response-bang-style';
+import ConsultingResponseStyle from './consulting-response-style';
 
 export default function ConsultingResponseRecommendStyle() {
   // const bangStyle = null;
   const bangStyle = BANG_STYLE.EXPOSED_FOREHEAD;
 
+  const value =
+    '(닉네임)님은 얼굴형이 이렇기 때문에, 끝이 가볍고 얼굴을 어느정도 가려줄 수 있는 레이어드컷이 잘 어울릴 것 같아요. 이런 느낌은 요새 많이 선택해주시기 때문에 짧은 기장이지만 부담없이 하실 수 있을 것 같습니다.';
+
+  const images = [
+    'https://meemong-job-storage.s3.ap-northeast-2.amazonaws.com/uploads/hair-consult-postings/images/2025/07/22/images/7b759976-7139-4f69-8e74-c8e6a4137d7c/7b759976-7139-4f69-8e74-c8e6a4137d7c.png',
+    'https://meemong-job-storage.s3.ap-northeast-2.amazonaws.com/uploads/hair-consult-postings/images/2025/07/22/images/7b759976-7139-4f69-8e74-c8e6a4137d7c/7b759976-7139-4f69-8e74-c8e6a4137d7c.png',
+    'https://meemong-job-storage.s3.ap-northeast-2.amazonaws.com/uploads/hair-consult-postings/images/2025/07/22/images/7b759976-7139-4f69-8e74-c8e6a4137d7c/7b759976-7139-4f69-8e74-c8e6a4137d7c.png',
+    'https://meemong-job-storage.s3.ap-northeast-2.amazonaws.com/uploads/hair-consult-postings/images/2025/07/22/images/7b759976-7139-4f69-8e74-c8e6a4137d7c/7b759976-7139-4f69-8e74-c8e6a4137d7c.png',
+    'https://meemong-job-storage.s3.ap-northeast-2.amazonaws.com/uploads/hair-consult-postings/images/2025/07/22/images/7b759976-7139-4f69-8e74-c8e6a4137d7c/7b759976-7139-4f69-8e74-c8e6a4137d7c.png',
+  ];
+
   return (
     <div className="flex flex-col gap-8">
       <ConsultingResponseBangStyle value={bangStyle} />
       <Separator />
+      <ConsultingResponseStyle value={value} images={images} />
     </div>
   );
 }
