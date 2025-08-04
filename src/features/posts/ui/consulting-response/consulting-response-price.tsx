466adb3b28d8244d0fa15f551dc074ce598f70a9
gaeun <knospe1@naver.com>
2025-08-04 16:36:05 +0900

    feat: 컨설팅 답변 코멘트 ui 추가

---
+++ src/features/posts/ui/consulting-response/consulting-response-price.tsx
@@ -0,0 +1,38 @@
+import ConsultingInputResultListItem from '../consulting-input-result-list-item';
+import ConsultingResponseItem from './consulting-response-item';
+
+type ConsultingResponsePriceProps = {
+  designerName: string;
+};
+
+export default function ConsultingResponsePrice({ designerName }: ConsultingResponsePriceProps) {
+  const prices = [
+    {
+      name: '레이어드 컷',
+      minPrice: 100000,
+      maxPrice: 200000,
+    },
+    {
+      name: '염색',
+      minPrice: 100000,
+      maxPrice: 200000,
+    },
+  ];
+
+  return (
+    <ConsultingResponseItem
+      title="시술 가격 견적"
+      content={`${designerName} 디자이너님께 시술 받으면 이 정도 견적으로 예상할 수 있어요`}
+    >
+      <div className="flex flex-col gap-4">
+        {prices.map((price, index) => (
+          <ConsultingInputResultListItem
+            key={`${price.name}-${price.minPrice}-${price.maxPrice}-${index}`}
+            name={price.name}
+            description={`${price.minPrice.toLocaleString()}원~${price.maxPrice.toLocaleString()}원`}
+          />
+        ))}
+      </div>
+    </ConsultingResponseItem>
+  );
+}
--- src/widgets/post/ui/consulting-response-price-and-comment-container.tsx
+++ src/widgets/post/ui/consulting-response-price-and-comment-container.tsx
@@ -1,3 +1,21 @@
+import ConsultingResponseComment from '@/features/posts/ui/consulting-response/consulting-response-comment';
+import ConsultingResponsePrice from '@/features/posts/ui/consulting-response/consulting-response-price';
+import { Separator } from '@/shared';
+
 export default function ConsultingResponsePriceAndCommentContainer() {
-  return <div>ConsultingResponsePriceAndComment</div>;
+  // const comment =
+  //   '(닉네임)님은 얼굴형이 이렇기 때문에, 끝이 가볍고 얼굴을 어느정도 가려줄 수 있는 레이어드컷이 잘 어울릴 것 같아요. 이런 느낌은 요새 많이 선택해주시기 때문에 짧은 기장이지만 부담없이 하실 수 있을 것 같습니다.';
+
+  const comment = null;
+  return (
+    <div className="flex flex-col gap-8">
+      <ConsultingResponsePrice designerName="익명" />
+      {comment && (
+        <>
+          <Separator />
+          <ConsultingResponseComment comment={comment} />
+        </>
+      )}
+    </div>
+  );
 }
