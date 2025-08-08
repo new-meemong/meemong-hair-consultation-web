5738f60f6fd141fb41df2babc99279df67716a3d
gaeun <knospe1@naver.com>
2025-08-08 23:59:40 +0900

    feat: 채팅 더보기 > 차단하기 모달 추가

--- src/features/chat/ui/chat-detail-more-button.tsx
+++ src/features/chat/ui/chat-detail-more-button.tsx
@@ -4,6 +4,7 @@ import MoreIcon from '@/assets/icons/more-horizontal.svg';
 import { useRouterWithUser } from '@/shared/hooks/use-router-with-user';
 import useLeaveChat from '../hook/use-leave-chat';
 import type { UserHairConsultationChatChannelType } from '../type/user-hair-consultation-chat-channel-type';
+import useShowModal from '@/shared/ui/hooks/use-show-modal';
 
 type ChatDetailMoreButtonProps = {
   chatChannel: UserHairConsultationChatChannelType;
@@ -11,6 +12,7 @@ type ChatDetailMoreButtonProps = {
 
 export default function ChatDetailMoreButton({ chatChannel }: ChatDetailMoreButtonProps) {
   const router = useRouterWithUser();
+  const showModal = useShowModal();
 
   const handleReport = useCallback(() => {
     router.push(ROUTES.REPORT(chatChannel.otherUser.id.toString()));
@@ -27,8 +29,31 @@ export default function ChatDetailMoreButton({ chatChannel }: ChatDetailMoreButt
   }, [handleLeaveChat, router, chatChannel]);
 
   const handleBlock = useCallback(() => {
-    console.log('차단하기');
-  }, []);
+    showModal({
+      id: 'block-chat-channel-modal',
+      text: `정말 ${chatChannel.otherUser.DisplayName}님을 차단하시겠습니까?\n차단 해제는 불가합니다.`,
+      buttons: [
+        {
+          label: '차단하기',
+          onClick: () => {
+            showModal({
+              id: 'block-chat-channel-confirm-modal',
+              text: '차단되었습니다.',
+              buttons: [
+                {
+                  label: '확인',
+                },
+              ],
+            });
+          },
+          textColor: 'text-negative',
+        },
+        {
+          label: '취소',
+        },
+      ],
+    });
+  }, [chatChannel.otherUser.DisplayName, showModal]);
 
   const moreOptions = useMemo(
     () => [
