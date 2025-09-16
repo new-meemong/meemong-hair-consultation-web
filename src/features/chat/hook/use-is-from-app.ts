62cd101e526b02e8d91f1f25e17ab87b8996fa93
gaeun <knospe1@naver.com>
2025-09-16 23:44:49 +0900

    fix: 재배포를 위한 코드수정

--- src/features/chat/hook/use-is-from-app.ts
+++ src/features/chat/hook/use-is-from-app.ts
@@ -5,6 +5,5 @@ import { SEARCH_PARAMS } from '@/shared/constants/search-params';
 export default function useIsFromApp() {
   const searchParams = useSearchParams();
   const isFromApp = searchParams.get(SEARCH_PARAMS.SOURCE) === 'app';
-
   return isFromApp;
 }
