be4913990e8dde7dcc76e4f92105aaadc7fe987a
gaeun <knospe1@naver.com>
2025-09-19 23:13:01 +0900

    fix: user role 판별 로직 채팅 유저도 가능하도록 수정

--- src/entities/user/lib/user-role.ts
+++ src/entities/user/lib/user-role.ts
@@ -1,10 +1,18 @@
+import type { UserHairConsultationChatChannelTypeOtherUser } from '@/features/chat/type/user-hair-consultation-chat-channel-type';
+
 import { USER_ROLE } from '../constants/user-role';
 import type { User } from '../model/user';
 
-export function isModel(user: Pick<User, 'role'>): boolean {
-  return user.role === USER_ROLE.MODEL;
+export function isModel(user: User | UserHairConsultationChatChannelTypeOtherUser): boolean {
+  if ('role' in user) {
+    return user.role === USER_ROLE.MODEL;
+  }
+  return user.Role === USER_ROLE.MODEL;
 }
 
-export function isDesigner(user: Pick<User, 'role'>): boolean {
-  return user.role === USER_ROLE.DESIGNER;
+export function isDesigner(user: User | UserHairConsultationChatChannelTypeOtherUser): boolean {
+  if ('role' in user) {
+    return user.role === USER_ROLE.DESIGNER;
+  }
+  return user.Role === USER_ROLE.DESIGNER;
 }
