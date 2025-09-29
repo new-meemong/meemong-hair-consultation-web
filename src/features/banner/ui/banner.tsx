02ac36d367b0f35cb1135466186f6ed75f6150cb
gaeun <knospe1@naver.com>
2025-09-30 01:47:56 +0900

    fix: 채팅 베너 클릭시 앱에서 링크 오픈하도록 수정

--- src/features/banner/ui/banner.tsx
+++ src/features/banner/ui/banner.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image';
 
 import type { Banner } from '@/entities/banner/model/banner';
+import openUrlInApp from '@/shared/lib/open-url-in-app';
 
 type BannerProps = {
   banner: Banner;
 };
 
 export default function Banner({ banner }: BannerProps) {
+  const handleClick = () => {
+    openUrlInApp(banner.redirectUrl);
+  };
+
   return (
     <Image
       src={banner.imageUrl}
@@ -20,6 +25,7 @@ export default function Banner({ banner }: BannerProps) {
         height: 'auto',
         boxShadow: '0 2px 2px 2px rgba(0, 0, 0, 0.2)',
       }}
+      onClick={handleClick}
     />
   );
 }
