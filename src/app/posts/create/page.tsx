72685346906f87a71511ab374762c4c6522b3307
gaeun <knospe1@naver.com>
2025-10-14 22:57:03 +0900

    fix: 컨설팅 게시글 form 수정

--- src/app/posts/create/page.tsx
+++ src/app/posts/create/page.tsx
@@ -2,20 +2,13 @@
 
 import { useState } from 'react';
 
-import { FormProvider } from 'react-hook-form';
-
 import { CONSULT_TYPE } from '@/entities/posts/constants/consult-type';
-import useConsultingPostForm from '@/features/posts/hooks/use-consulting-post-form';
-import usePostFormNavigation from '@/features/posts/hooks/use-post-form-navigation';
 import { usePostTab } from '@/features/posts/hooks/use-post-tab';
-import type { ConsultingPostFormValues } from '@/features/posts/types/consulting-post-form-values';
-import type { WritingStep } from '@/features/posts/types/user-writing-content';
-import ConsultingPostForm from '@/features/posts/ui/consulting-form/consulting-post-form/consulting-post-form';
-import PostForm from '@/features/posts/ui/post-form/post-form';
 import { USER_GUIDE_KEYS } from '@/shared/constants/local-storage';
 import useShowGuide from '@/shared/hooks/use-show-guide';
 import type { ValueOf } from '@/shared/type/types';
-import { SiteHeader } from '@/widgets/header';
+import ConsultingPostFormContainer from '@/widgets/post/ui/consulting-post-form-container';
+import PostFormContainer from '@/widgets/post/ui/post-form-container';
 
 export default function CreatePostPage() {
   useShowGuide(USER_GUIDE_KEYS.hasSeenCreatePostGuide);
@@ -24,66 +17,20 @@ export default function CreatePostPage() {
 
   const [selectedTab] = usePostTab();
 
-  const [currentStep, setCurrentStep] = useState(1);
-
-  const { method, submit: submitConsultingForm } = useConsultingPostForm();
-
-  const { isDirty } = method.formState;
-
-  const handlePageReload = (savedContent: WritingStep<ConsultingPostFormValues>) => {
-    if (!savedContent) return;
-
-    setCurrentStep(savedContent.step);
-    method.reset(savedContent.content);
-  };
-
-  const { leaveForm } = usePostFormNavigation({
-    onSavedContentReload: handlePageReload,
-  });
-
-  const handleBackClick = () => {
-    const writingContent: WritingStep<ConsultingPostFormValues> = {
-      step: currentStep,
-      content: method.getValues(),
-    };
-
-    leaveForm(writingContent, isDirty);
-  };
-
   const renderForm = (type: ValueOf<typeof CONSULT_TYPE>) => {
     switch (type) {
       case CONSULT_TYPE.CONSULTING:
-        return (
-          <ConsultingPostForm
-            currentStep={currentStep}
-            setCurrentStep={setCurrentStep}
-            onSubmit={submitConsultingForm}
-          />
-        );
+        return <ConsultingPostFormContainer />;
       case CONSULT_TYPE.GENERAL:
-        return <PostForm />;
+        return <PostFormContainer />;
       default:
         return null;
     }
   };
 
-  const getTitle = (type: ValueOf<typeof CONSULT_TYPE>) => {
-    switch (type) {
-      case CONSULT_TYPE.CONSULTING:
-        return '상담지 작성';
-      case CONSULT_TYPE.GENERAL:
-        return '일반글 작성';
-      default:
-        return '';
-    }
-  };
-
   return (
     <div className="h-screen bg-white flex flex-col min-h-0" style={{ minHeight: initialHeight }}>
-      <FormProvider {...method}>
-        <SiteHeader title={getTitle(selectedTab)} showBackButton onBackClick={handleBackClick} />
-        {renderForm(selectedTab)}
-      </FormProvider>
+      {renderForm(selectedTab)}
     </div>
   );
 }
---
+++ src/features/posts/hooks/use-consulting-post-form-navigation.ts
@@ -0,0 +1,65 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { USER_WRITING_CONTENT_KEYS } from '@/shared/constants/local-storage';
+import { useRouterWithUser } from '@/shared/hooks/use-router-with-user';
+import useWritingContent from '@/shared/hooks/use-writing-content';
+
+import type { ConsultingPostFormValues } from '../types/consulting-post-form-values';
+import type { WritingStep } from '../types/user-writing-content';
+
+import useShowLeaveCreateConsultingPostModal from './use-show-leave-create-consulting-post';
+import useShowReloadConsultingPostModal from './use-show-reload-consulting-post-modal';
+
+export default function usePostFormNavigation({
+  onSavedContentReload,
+}: {
+  onSavedContentReload: (savedContent: WritingStep<ConsultingPostFormValues>) => void;
+}) {
+  const [initialize, setInitialize] = useState(false);
+
+  const { savedContent, saveContent } = useWritingContent(USER_WRITING_CONTENT_KEYS.consultingPost);
+  const hasSavedContent = savedContent !== null;
+
+  const { back } = useRouterWithUser();
+
+  const handleCloseReloadConsultingPostModal = () => {
+
+  };
+
+  const showReloadConsultingPostModal = useShowReloadConsultingPostModal({
+    onClose: handleCloseReloadConsultingPostModal,
+    onPositive: () => onSavedContentReload(savedContent),
+    onNegative: () => {
+      saveContent(null);
+    },
+  });
+  const showLeaveCreateConsultingPostModal = useShowLeaveCreateConsultingPostModal();
+
+  useEffect(() => {
+    if (hasSavedContent && !initialize ) {
+      showReloadConsultingPostModal();
+    }
+    setInitialize(true);
+  }, [showReloadConsultingPostModal, initialize, hasSavedContent]);
+
+  const leaveForm = useCallback(
+    (writingContent: WritingStep<ConsultingPostFormValues>, isDirty: boolean) => {
+      if (savedContent || isDirty) {
+        showLeaveCreateConsultingPostModal({
+          onClose: () => {
+            back();
+            saveContent(writingContent);
+          },
+        });
+        return;
+      }
+
+      back();
+
+    },
+    [back, savedContent, showLeaveCreateConsultingPostModal, saveContent],
+  );
+
+
+  return { leaveForm };
+}
