156ba22afd8b451e744ad9dd52deb64e13216529
gaeun <knospe1@naver.com>
2025-10-19 14:47:48 +0900

    fix: 사용하지 않는 변수 삭제

--- src/features/comments/ui/comment-form.tsx
+++ src/features/comments/ui/comment-form.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 
-import { FormProvider, useForm, useWatch } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import z from 'zod';
