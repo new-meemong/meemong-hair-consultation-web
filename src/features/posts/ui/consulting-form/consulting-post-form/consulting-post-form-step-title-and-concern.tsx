a7b164ae4321a4875f7c402aef1f1d075154db46
gaeun <knospe1@naver.com>
2025-10-31 14:34:24 +0900

    fix: 컨설팅 게시글 validation 에러 수정

--- src/features/posts/ui/consulting-form/consulting-post-form/consulting-post-form-step-title-and-concern.tsx
+++ src/features/posts/ui/consulting-form/consulting-post-form/consulting-post-form-step-title-and-concern.tsx
@@ -9,6 +9,9 @@ import { Input, Textarea } from '@/shared';
 export default function ConsultingPostFormStepTitleAndConcern() {
   const { register } = useFormContext<ConsultingPostFormValues>();
   const concernRef = useRef<HTMLTextAreaElement | null>(null);
+  const { ref: concernRegisterRef, ...concernRegister } = register(
+    `${CONSULTING_POST_FORM_FIELD_NAME.CONCERN}.additional`,
+  );
 
   return (
     <div className="flex flex-col gap-4">
@@ -25,10 +28,13 @@ export default function ConsultingPostFormStepTitleAndConcern() {
         <span className="typo-body-2-semibold text-cautionary">필수</span>
       </div>
       <Textarea
-        {...register(`${CONSULTING_POST_FORM_FIELD_NAME.CONCERN}.additional`)}
+        {...concernRegister}
         placeholder="헤어고민/요청사항을 간단하게 입력해주세요"
         className="min-h-38"
-        ref={concernRef}
+        ref={(el) => {
+          concernRegisterRef(el);
+          concernRef.current = el;
+        }}
       />
     </div>
   );
