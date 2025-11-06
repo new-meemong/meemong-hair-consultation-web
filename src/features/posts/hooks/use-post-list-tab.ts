6da4a5f96d62254d4b5e1b2ef3a560965c4d86d8
gaeun <knospe1@naver.com>
2025-11-06 09:42:15 +0900

    fix: 목록 페이지 리스트탭 클릭 시 게시글 탭도 유지되도록 수정

--- src/features/posts/hooks/use-post-list-tab.ts
+++ src/features/posts/hooks/use-post-list-tab.ts
@@ -1,7 +1,9 @@
 import { useSearchParams } from 'next/navigation';
 
+import { CONSULT_TYPE } from '@/entities/posts/constants/consult-type';
 import { SEARCH_PARAMS } from '@/shared/constants/search-params';
 import { useRouterWithUser } from '@/shared/hooks/use-router-with-user';
+import type { ValueOf } from '@/shared/type/types';
 
 import type { PostListTab } from '../types/post-list-tab';
 
@@ -12,10 +14,14 @@ export default function usePostListTab() {
 
   const activePostListTab =
     (searchParams.get(SEARCH_PARAMS.POST_LIST_TAB) as PostListTab) ?? 'latest';
+  const activePostTab =
+    (searchParams.get(SEARCH_PARAMS.POST_TAB) as ValueOf<typeof CONSULT_TYPE>) ??
+    CONSULT_TYPE.CONSULTING;
 
   const setActivePostListTab = (tab: PostListTab) => {
     router.replace(window.location.pathname, {
       [SEARCH_PARAMS.POST_LIST_TAB]: tab,
+      [SEARCH_PARAMS.POST_TAB]: activePostTab,
     });
   };
 
