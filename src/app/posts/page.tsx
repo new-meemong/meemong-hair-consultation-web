3c64f1d8884d2b025b8c475ce77a7dd832a8a1c0
Saehanseul <saehanseul@gmail.com>
2025-12-07 16:24:09 +0900

    feat: 배너띠 고정 안되고 스크롤 되도록 수정

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -77,8 +77,8 @@ export default function PostsPage() {
       <SiteHeader title="헤어상담" />
       <div className="flex flex-col flex-1 min-h-0">
         <Tab options={postTabs} value={activePostTab} onChange={setActivePostTab} />
-        <PromotionBanner />
         <div ref={containerRef} className="flex flex-col gap-5 flex-1 overflow-y-auto">
+          <PromotionBanner />
           <TopAdvisorCarousel />
           <div className="flex-1 flex flex-col min-h-0 gap-2">
             <div className="flex-shrink-0">
