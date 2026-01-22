a929a1f18fb84f7e14200770048cb246c3748560
Saehanseul <saehanseul@gmail.com>
2026-01-22 19:08:10 +0900

    feat: 다른 모델 프로필 접근 못하게 수정

--- src/features/posts/ui/post-detail/post-detail-author-profile.tsx
+++ src/features/posts/ui/post-detail/post-detail-author-profile.tsx
@@ -1,10 +1,11 @@
-import ProfileIcon from '@/assets/icons/profile.svg';
-import { useAuthContext } from '@/features/auth/context/auth-context';
-import formatAddress from '@/features/auth/lib/format-address';
 import { Avatar, AvatarFallback, AvatarImage } from '@/shared';
-import { useShowInvalidChatRequestSheet } from '@/features/chat/hook/use-show-invalid-chat-request-sheet';
-import goModelProfilePage from '@/shared/lib/go-model-profile-page';
+
+import ProfileIcon from '@/assets/icons/profile.svg';
 import { cn } from '@/lib/utils';
+import formatAddress from '@/features/auth/lib/format-address';
+import goModelProfilePage from '@/shared/lib/go-model-profile-page';
+import { useAuthContext } from '@/features/auth/context/auth-context';
+import { useShowInvalidChatRequestSheet } from '@/features/chat/hook/use-show-invalid-chat-request-sheet';
 
 type PostDetailAuthorProfileProps = {
   imageUrl: string | null;
@@ -21,14 +22,17 @@ export default function PostDetailAuthorProfile({
   createdAt,
   authorId,
 }: PostDetailAuthorProfileProps) {
-  const { user, isUserDesigner } = useAuthContext();
+  const { user, isUserDesigner, isUserModel } = useAuthContext();
   const showInvalidChatRequestBottomSheet = useShowInvalidChatRequestSheet();
 
   const shouldShowRegion = isUserDesigner && region;
   const isWriter = user.id === authorId;
 
+  const isClickable = !isWriter && !isUserModel;
+
   const handleClick = () => {
     if (isWriter) return;
+    if (isUserModel) return;
 
     if (isUserDesigner) {
       showInvalidChatRequestBottomSheet();
@@ -40,7 +44,7 @@ export default function PostDetailAuthorProfile({
 
   return (
     <div
-      className={cn('flex items-center gap-2', !isWriter && 'cursor-pointer')}
+      className={cn('flex items-center gap-2', isClickable && 'cursor-pointer')}
       onClick={handleClick}
     >
       <Avatar>
