c3c9ed0a17a53d068d0d887b45957c834d3dbcce
Saehanseul <saehanseul@gmail.com>
2026-02-24 12:20:12 +0900

    feat: 문구 오타 수정

--- src/features/posts/hooks/use-show-reload-saved-post-form-modal.ts
+++ src/features/posts/hooks/use-show-reload-saved-post-form-modal.ts
@@ -2,9 +2,9 @@ import { USER_WRITING_CONTENT_KEYS } from '@/shared/constants/local-storage';
 import useShowModal from '@/shared/ui/hooks/use-show-modal';
 
 const TEXT = {
-  [USER_WRITING_CONTENT_KEYS.consultingPost]: '컨설팅',
-  [USER_WRITING_CONTENT_KEYS.hairConsultation]: '신규 상담지',
-  [USER_WRITING_CONTENT_KEYS.experienceGroup]: '체험단 신청',
+  [USER_WRITING_CONTENT_KEYS.consultingPost]: '컨설팅이',
+  [USER_WRITING_CONTENT_KEYS.hairConsultation]: '신규 상담지가',
+  [USER_WRITING_CONTENT_KEYS.experienceGroup]: '체험단 신청이',
 };
 
 export default function useShowReloadSavedPostFormModal({
@@ -30,7 +30,7 @@ export default function useShowReloadSavedPostFormModal({
   } = {}) => {
     showModal({
       id: 'reload-consulting-post-modal',
-      text: `작성중인 ${TEXT[type]}이 있습니다.\n이어서 작성하시겠습니까?`,
+      text: `작성중인 ${TEXT[type]} 있습니다.\n이어서 작성하시겠습니까?`,
       buttons: [
         {
           label: '이어서 작성하기',
